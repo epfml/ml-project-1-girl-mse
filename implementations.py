08dcc9062ea6100693d8e6eca1ef512a1097f61e
elenamrdja <91255937+elena-mrdja@users.noreply.github.com>
2023-10-30 01:26:56 +0100

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -85,6 +85,8 @@ def compute_stoch_gradient(y, tx, w):
     Returns:
         grad: vector of shape=(D,)
     """
+    y = y.flatten()
+    w = w.flatten()
     error = y - np.dot(tx, w)
     grad = (-1)*np.dot(tx.T, error)
     
